467618778397bf8a58f2f82ba2a1404866f147a0
Juliane Campos <64619650+julianecampos@users.noreply.github.com>
2022-06-01 18:24:34 -0300

    Add files via upload

---
+++ src/main/java/com/goodbrowsergames/backend/controller/AvaliacaoController.java
@@ -0,0 +1,32 @@
+package com.goodbrowsergames.backend.controller;
+
+import com.goodbrowsergames.backend.entity.Avaliacao;
+import com.goodbrowsergames.backend.entity.Jogo;
+import com.goodbrowsergames.backend.service.AvaliacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/goodbrowsergames/avaliacao")
+@CrossOrigin("*")
+public class AvaliacaoController {
+    @Autowired
+    private AvaliacaoService avaliacaoService;
+
+    @GetMapping("/listar")
+    public List<Avaliacao> lsitarAvaliacao() { return avaliacaoService.readAll(); }
+
+    @PostMapping("/qtdDeAvaliacao/{idUsuario}/{nota}")
+    public Jogo jogo(@RequestBody Jogo jogo,
+                     @PathVariable Integer idUsuario,
+                     @PathVariable Integer nota) {
+        return avaliacaoService.qtdDeAvaliacao(jogo, idUsuario, nota);
+    }
+
+    @GetMapping("/listarPorIdJogo/{idJogo}")
+    public List<Avaliacao> lsitarAvaliacaoPorIdJogo(@PathVariable Integer idJogo) {
+        return avaliacaoService.visualizarOutrasAvaliacoes(idJogo);
+    }
+}
